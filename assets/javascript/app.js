71bd8fa8fab7fe2fa8f4307f526ffa52d2fc4371
itsmegh <42684917+itsmegh@users.noreply.github.com>
2018-10-16 13:48:09 -0600

    Added questions and answers on html, added simple page design with bootstrap

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,11 @@
-
+//Trivia form with multiple choice or true/false questions
+//Player has to finish the quiz in a limited amount of time
+    //game ends when time runs out
+    //end of game page shows number of questions the player got correct and incorrect
+//Game Play
+    //Player can only pick one answer per question
+    //show a countdown timer
+    //identify correct answer chosen
 
 $(document).ready(function() {
 
