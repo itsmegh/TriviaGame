347d20254053fd26d9a9654a7a4ca8192f3cecea
itsmegh <42684917+itsmegh@users.noreply.github.com>
2018-10-16 15:02:40 -0600

    Added stopwatch code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,10 +14,62 @@
         //show win or loss based on corrected answers (8 total questions)
         //button option to restart the game
 $(document).ready(function() {
-
-
+// WANT MODAL TO LOAD BEFORE THE CLOCK COUNTER STARTS - HOW TO?
 // link to modal information https://getbootstrap.com/docs/4.1/components/modal/
 //create modal with "start button" and then at the end of the game the modal will appear again with game score and the start button.
 //add functionality for ".button-finish" - show modal with questions correct/wrong/unanswered/start button?
 
+    window.onload = function() {
+        //we don't want an onclick event, we just want it to start when the modal clicks off... how?
+    }
+
+    var intervalID;
+    var clockRunning = false;
+    var stopwatch = {
+        time: 0,
+
+        reset: function() {
+            stopwatch.time = 0;
+
+            $("#display").text("00:00");
+        },
+
+        start: function() {
+            if(!clockRunning) {
+                intervalID = setInterval(stopwatch.count, 1000);
+                clockRunning = true;
+            }
+        },
+
+        stop: function() {
+            clearInterval(intervalID);
+            clockRunning = false;
+        },
+
+        count: function() {
+            stopwatch.time++;
+            var currentTime = stopwatch.timeConverter(stopwatch.time);
+            $("#display").text(currentTime);
+        },
+
+        timeConverter: function(t) {
+            var minutes = Math.floor(t / 60);
+            var seconds = t - (minutes * 60);
+
+            if (seconds < 10) {
+                seconds = "0" + seconds;
+            }
+
+            if  (minutes === 0) {
+                minutes = "00";
+            }
+            
+            else if (minutes < 10) {
+                minutes = "0" + minutes;
+            }
+
+            return minutes + ":" + seconds;
+        }
+         
+    }
 });
