0891ec8fbf0c8c896eb8961c92225e9af8fe0e7c
itsmegh <42684917+itsmegh@users.noreply.github.com>
2018-10-16 14:20:16 -0600

    pseudo coding on javascript page, set up button on html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,23 @@
-//Trivia form with multiple choice or true/false questions
+//Trivia form with multiple choice or true/false questions - done!
 //Player has to finish the quiz in a limited amount of time
     //game ends when time runs out
     //end of game page shows number of questions the player got correct and incorrect
 //Game Play
     //Player can only pick one answer per question
     //show a countdown timer
-    //identify correct answer chosen
+    //identify answers chosen - and count in the background correct versus incorrect
 
+//Game start and finish
+    //need to add a cover page that hides the questions - start button
+    // done button at the bottom of the questions to stop the game clock
+    //show cover page again, but this time with correct, incorrect and unanswered questions
+        //show win or loss based on corrected answers (8 total questions)
+        //button option to restart the game
 $(document).ready(function() {
+
+
+// link to modal information https://getbootstrap.com/docs/4.1/components/modal/
+//create modal with "start button" and then at the end of the game the modal will appear again with game score and the start button.
+//add functionality for ".button-finish" - show modal with questions correct/wrong/unanswered/start button?
 
 });
